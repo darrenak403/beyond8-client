c61740444ee1335de1acbb5407a36e38a2966ae5
darrenak403 <ngothanhdat4002@gmail.com>
2026-01-26 14:18:43 +0700

    exclude xml files from middleware

--- middleware.ts
+++ middleware.ts
@@ -230,8 +230,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - public files (images, etc.)
+     * - public files (images, xml, etc.)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|webm|mp4)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|webm|mp4|xml)$).*)',
   ],
 }
